c27928a4d0aa77de7bfb6575fb9ac339ee99f792
Vasily Mikhaylovsky <git@3apaxi.com>
2017-02-01 17:09:27 +0500

    Present every day of my life

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <hello></hello>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="stylus">
+html
+  margin 0
+  padding 0
+  font-size 15px
+  width 100%
+  height 100%
+  display flex
+  align-items center
+  align-content center
+  justify-content center
+body
+  margin 0
+  padding 0
+
+</style>
---
+++ src/components/Day.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  .day(:class="className", :title="day")
+</template>
+
+<script>
+export default {
+  name: 'day',
+  props: ['day', 'lived'],
+  computed: {
+    className () {
+      if (this.day === this.lived) {
+        return 'today'
+      }
+      if (this.day < this.lived) {
+        return 'past'
+      }
+      return 'future'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus">
+.day
+  width 1em
+  height 1em
+  overflow hidden
+  font-size 50%
+  //display block
+  //float left
+  flex 0 0 auto
+  margin-left -1px
+  margin-top -1px
+.past
+  background #302E2E
+.today
+  background #FFE0BD
+  animation blink-animation 1s steps(2, start) infinite
+.future
+  background #FFE0BD
+@keyframes blink-animation {
+  to {
+    background #302E2E
+  }
+}
+</style>
