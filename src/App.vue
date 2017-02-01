f403dab9e004af71aa53e5cea2099f6d2ca7bd3c
Vasily Mikhaylovsky <git@3apaxi.com>
2017-02-01 17:48:36 +0500

    move vue hello to app; fix center

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,48 @@
-<template>
-  <hello></hello>
+<template lang="pug">
+  .root
+    h1 {{percent}}%
+    .days
+      Day(v-for="d in days", :day="d", :lived="lived")
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Day from './components/Day'
+import moment from 'moment'
+import {CronJob} from 'cron'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      birthday: moment('1985-03-13T00:00:01'),
+      now: moment()
+    }
+  },
+  methods: {
+    updateNow () {
+      this.now = moment()
+    }
+  },
+  created () {
+    /* eslint-disable no-new */
+    new CronJob('0 0 0 * * *', () => this.updateNow(), null, true)
+  },
+  computed: {
+    last () {
+      return moment(this.birthday).add(60, 'years')
+    },
+    days () {
+      return this.last.diff(this.birthday, 'days')
+    },
+    lived () {
+      return this.now.diff(this.birthday, 'days')
+    },
+    percent () {
+      return Number(100 * this.lived / this.days).toFixed(1)
+    }
+  },
   components: {
-    Hello
+    Day
   }
 }
 </script>
@@ -21,11 +55,29 @@ html
   width 100%
   height 100%
   display flex
+body
+  flex 1 0 auto
+  margin 0
+  padding 0
+  width 100%
+  display flex
+.root
+  flex 0 1 auto
+  width 100%
+  display flex
+  flex-direction column
   align-items center
   align-content center
   justify-content center
-body
-  margin 0
-  padding 0
-
-</style>
+h1
+  text-align center
+  flex 0 0 auto
+.days
+  flex 0 1 auto
+  display flex
+  flex-wrap wrap
+  //align-items center
+  //align-content center
+  //justify-content center
+  margin-left 1px
+  margin-top 1px</style>
